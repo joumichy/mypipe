b073ac85662f2cd9670a9d671f4b975ddf16ee06
joumichy <allou.john@hotmail.fr>
2019-12-19 09:39:54 +0100

    main updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+
+extern crate clap;
+use clap::{Arg, App};
+use std::process::Command;
+
+
 fn main() {
     println!("Hello, world!");
+    let matches = App::new("mypipe")
+                .version("1.0")
+                .author("joumichy. <allou.john@hotmail.fr>")
+                .about("Pipe implementation")
+                .arg(Arg::with_name("input")
+                    .short("i")
+                    .long("--in")
+                    .help("That is the input for our pipe")
+                    .required(true)
+                    .takes_value(true)
+                )
+                .arg(Arg::with_name("output")
+                    .short("o")
+                    .long("--out")
+                    .help("That is the output for our pipe")
+                    .required(true)
+                    .takes_value(true)
+                )                      
+                .get_matches();
+
+    let _input = matches.value_of("input").unwrap_or("hello");
+    let _output = matches.value_of("output").unwrap_or("world");
+
+    let cmd_input = Command::new(_input.to_string())
+                        .output()
+                        .expect("An error occurs during the input process");
+
+    let cmd_output = Command::new(_output.to_string())
+                        .arg(String::from_utf8_lossy(&cmd_input.stdout).to_string())
+                        .output()
+                        .expect("An error occurs during the output process");
+
+    println!("{}", String::from_utf8_lossy(&cmd_output.stdout).to_string());
+
 }
